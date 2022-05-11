278678edb808c35c6486c48a8c8dba94e6491353
depriandiy <depriandianton22@gmail.com>
2022-05-11 09:36:03 +0700

    W10 - Homework

--- app/src/main/java/com/ubaya/todoapp_160419060/util/Util.kt
+++ app/src/main/java/com/ubaya/todoapp_160419060/util/Util.kt
@@ -8,17 +8,17 @@ import com.ubaya.todoapp_160419060.model.TodoDatabase
 
 val DB_NAME = "newtododb"
 
-val MIGRATION_1_2 = object : Migration(1, 2) {
+val MIGRATION_1_3 = object : Migration(1, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
-            "ALTER TABLE todo ADD COLUMN priority INTEGER DEFAULT 3 not null"
+            "ALTER TABLE todo ADD COLUMN is_done INTEGER DEFAULT 0 not null"
         )
     }
 }
 
 fun buildDb(context: Context): TodoDatabase {
     val db = Room.databaseBuilder(context, TodoDatabase::class.java, DB_NAME)
-        .addMigrations(MIGRATION_1_2)
+        .addMigrations(MIGRATION_1_3)
         .build()
 
     return db
