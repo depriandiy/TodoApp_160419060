f5969e34fa09e6174fd2624aa9bc8d5ac0204b8d
depriandiy <depriandianton22@gmail.com>
2022-05-11 09:46:25 +0700

    W10 - Homework.

--- app/src/main/java/com/ubaya/todoapp_160419060/model/Model.kt
+++ app/src/main/java/com/ubaya/todoapp_160419060/model/Model.kt
@@ -13,7 +13,8 @@ data class Todo(
     @ColumnInfo(name = "priority")
     var priority:Int,
     @ColumnInfo(name = "is_done")
-    var is_done:Int //
+    var is_done:Int //maybe because in MySQL don't have boolean data type
+    // and instead convert boolean values into integers (1=TRUE, 0 = FALSE)
 ) {
     @PrimaryKey(autoGenerate = true)
     var uuid: Int = 0
